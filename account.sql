ac210c57f938b83e0881c4f4eb9e6241306c02b0
Amit Junankar <55340245+amuejunankar@users.noreply.github.com>
2023-04-05 00:49:15 +0530

    Added Updated DB

--- account.sql
+++ account.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2023 at 05:32 PM
+-- Generation Time: Apr 04, 2023 at 09:18 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -37,17 +37,9 @@ CREATE TABLE `accounttable` (
   `profilephoto` blob NOT NULL,
   `is_verified` int(10) DEFAULT 0,
   `token` varchar(100) DEFAULT NULL,
-  `otp` varchar(10) DEFAULT NULL
+  `otp` varchar(10) DEFAULT NULL,
+  `otpsms` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `accounttable`
---
-
-INSERT INTO `accounttable` (`id`, `name`, `email`, `mobilecode`, `mobilenumber`, `password`, `profilephoto`, `is_verified`, `token`, `otp`) VALUES
-(37, 'Amit', 'cibaba@lyft.live', 91, '8668635805', 'asdefrftg', '', 1, '0', '0'),
-(38, 'new 12', 'javocata@tutuapp.bid', 91, '4684464684', '748596', '', 1, '0', '0'),
-(39, 'test2', '5jc29qaph19@qiott.com', 91, '7875733150', 'finally', '', 1, '0', '0');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +59,7 @@ ALTER TABLE `accounttable`
 -- AUTO_INCREMENT for table `accounttable`
 --
 ALTER TABLE `accounttable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
