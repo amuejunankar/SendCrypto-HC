3bee2fe0b0e3b1e5dcafd800fe59430e65646419
Amit Junankar <55340245+amuejunankar@users.noreply.github.com>
2023-04-11 23:02:20 +0530

    profile Upload And Display is working

--- html/account/account.php
+++ html/account/account.php
@@ -18,8 +18,37 @@ if (isset($_POST['logout'])) {
     exit;
 }
 
-?>
+// T O   D O
 
+// Import Connection Files
+
+include '../../database/connection.php';
+$conn = connect();
+
+// Retrieve profile picture from database and output as HTML
+$email = 'junankgg@gmail.com';
+$sql = "SELECT profile_picture FROM accounttable WHERE email = ?"; // Replace with your appropriate query
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "s", $email);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $profile_picture = $row["profile_picture"];
+    // encode the image data as base64 for use in the data URI
+    $base64_image = base64_encode($profile_picture);
+    $image_src = 'data:image/png;base64,' . $base64_image; // replace "png" with the appropriate image format
+    // output the HTML code with the profile picture
+} else {
+    echo "0 results";
+}
+
+// Upload Image File
+
+// Update Form
+
+?>
 
 
 <!DOCTYPE html>
@@ -74,45 +103,69 @@ if (isset($_POST['logout'])) {
 
     <!-- Add your HTML content here -->
 
-
-
-
     <div class="profile-info" style="margin-left: 42%;
                                     margin-top: 1%;">
-        <form>
-            <div class="profile-header">
-                <h1>Profile</h1>
-            </div>
-            <div class="profile-container">
-                <div class="profile-picture-container form-group">
-                    <img src="../../src/default-profile-picture.png" alt="Profile Picture">
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <title>Profile Form</title>
+        </head>
+
+        <body>
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                <div class="profile-header">
+                    <h1>Profile</h1>
                 </div>
-                <div class="form-group">
-                    <label for="first-name">First Name:</label>
-                    <input value="First demo" type="text" id="first-name" name="first-name" required>
+                <div class="profile-container">
+                    <div class="profile-picture-container form-group">
+                        <img src="<?= $image_src ?>" alt="Profile Picture" id="profile-picture">
+                        <input type="file" id="profile-picture-input" accept="image/*" name="imageFile">
+                    </div>
+                    <div class="form-group">
+                        <label for="first-name">First Name:</label>
+                        <input value="First demo" type="text" id="first-name" name="first-name" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="last-name">Last Name:</label>
+                        <input value="Last demo" type="text" id="last-name" name="last-name" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input value="demo@gmail.com" type="email" id="email" name="email" required disabled>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="mobile-number">Mobile Number:</label>
+                        <input value="12345678" type="tel" id="mobile-number" name="mobile-number" required disabled>
+                    </div>
+
                 </div>
+                <button class="save-btn" type="submit">Save Changes</button>
+            </form>
 
-                <div class="form-group">
-                    <label for="last-name">Last Name:</label>
-                    <input value="Last demo" type="text" id="last-name" name="last-name" required>
-                </div>
+            <script>
+                const profilePicture = document.getElementById('profile-picture');
+                const profilePictureInput = document.getElementById('profile-picture-input');
 
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input value="demo@gmail.com" type="email" id="email" name="email" required disabled>
-                </div>
+                profilePicture.addEventListener('click', () => {
+                    profilePictureInput.click();
+                });
 
-                <div class="form-group">
-                    <label for="mobile-number">Mobile Number:</label>
-                    <input value="12345678" type="tel" id="mobile-number" name="mobile-number" required disabled>
-                </div>
+                profilePictureInput.addEventListener('change', (event) => {
+                    const file = event.target.files[0];
+                    const reader = new FileReader();
+                    reader.readAsDataURL(file);
+                    reader.onload = () => {
+                        profilePicture.src = reader.result;
+                    };
+                });
+            </script>
+        </body>
 
-            </div>
-            <button class="save-btn" type="submit">Save Changes</button>
-        </form>
-    </div>
-
-
+        </html>
 
 
 
---
+++ html/account/upload.php
@@ -0,0 +1,23 @@
+<?php
+include '../../database/connection.php';
+$conn = connect();
+
+// Check if file was uploaded
+if (isset($_FILES["imageFile"]) && $_FILES["imageFile"]["error"] == 0) {
+    $image = $_FILES["imageFile"]["tmp_name"];
+    $image_data = file_get_contents($image);
+
+    $email = 'junankgg@gmail.com'; // Get the email value from your form or another source
+    $sql = "UPDATE accounttable SET profile_picture = ? WHERE email = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $image_data, $email);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    echo "Image uploaded successfully and path updated in database.";
+}
+
+// Redirect back to account page
+header("Location: account.php");
+exit();
+?>
