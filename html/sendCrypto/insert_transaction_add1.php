076e81bccfc33197ac58d5cc1f56646321f501f7
Amit Junankar <55340245+amuejunankar@users.noreply.github.com>
2023-05-12 19:59:04 +0530

    w

--- html/sendCrypto/insert_transaction_add1.php
+++ html/sendCrypto/insert_transaction_add1.php
@@ -16,34 +16,26 @@ $mobileNumber = $_POST['mobile_number'];
 $operator = $_POST['operator'];
 $state = $_POST['state'];
 
-echo '<script>';
-echo 'console.log("to_address: ' . $to_address . '");';
-echo 'console.log("amount: ' . $amount . '");';
-echo 'console.log("tx_hash: ' . $tx_hash . '");';
-echo 'console.log("mobile_number: ' . $mobile_number . '");';
-echo 'console.log("operator: ' . $operator . '");';
-echo 'console.log("state: ' . $state . '");';
-echo '</script>';
-
 
 // Calculate the plan price based on the selected plan in the HTML form
 $plan_price = '399';
 
-$from_address = $mobile_number . ' ' . $operator . ' ' . $plan_price;
-
-
+$from_address = $mobileNumber . ' ' . $operator . ' ' . $plan_price;
 
 // Get user's email from session and sanitize it
 $email = mysqli_real_escape_string($conn, $_SESSION["email"]);
 
 // Prepare SQL statement and handle any errors
-$stmt = $conn->prepare("INSERT INTO transactions (from_address, to_address, amount, tx_hash, email) VALUES (?, ?, ?, ?, ?)");
-
+$stmt = $conn->prepare("INSERT INTO transactions (from_address, to_address, amount, tx_hash, email) VALUES (?, ?, ?, ?, ?) WHERE email = ?");
 if (!$stmt) {
     die("Error preparing statement: " . mysqli_error($conn));
 }
 
-$stmt->bind_param("sssss", $from_address, $to_address, $amount, $tx_hash, $email);
+// Bind parameters to statement and handle any errors
+$stmt->bind_param("ssssss", $from_address, $to_address, $amount, $tx_hash, $email, $email);
+if (!$stmt) {
+    die("Error binding parameters: " . mysqli_error($conn));
+}
 
 // Execute the SQL statement and handle any errors
 if ($stmt->execute()) {
@@ -52,10 +44,6 @@ if ($stmt->execute()) {
     echo "Error inserting transaction data: " . $stmt->error;
 }
 
-echo "Data received successfully!";
-
-
 // Close the prepared statement and database connection
 $stmt->close();
 mysqli_close($conn);
-?>
